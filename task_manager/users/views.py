65c5d114cd7b2107127d0e02df70b540afd89a29
qamaz <solgrini@yandex.ru>
2022-10-05 18:25:49 +0300

    crispy forms added

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -6,3 +6,11 @@ class UsersView(ListView):
     model = User
     context_object_name = "user_list"
     template_name = "users/user_list.html"
+
+
+class CreateView(ListView):
+    pass
+
+
+class LoginView(ListView):
+    pass
