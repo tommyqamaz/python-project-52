0cca68fa546fa63bde20f84b820aaf380a715dd4
qamaz <solgrini@yandex.ru>
2022-10-06 14:07:57 +0300

    base test added

--- task_manager/tests.py
+++ task_manager/tests.py
@@ -1,7 +1,30 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model, authenticate
 
 
-class AppTest(TestCase):
+class IndexTest(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="test", password="12test12", first_name="bla", last_name="blabla"
+        )
+        self.user.save()
+
     def test_availability(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "task_manager/index.html")
+
+    def tearDown(self):
+        self.user.delete()
+
+    def test_correct(self):
+        user = authenticate(username="test", password="12test12")
+        self.assertTrue((user is not None) and user.is_authenticated)
+
+    def test_wrong_username(self):
+        user = authenticate(username="wrong", password="12test12")
+        self.assertFalse(user is not None and user.is_authenticated)
+
+    def test_wrong_pssword(self):
+        user = authenticate(username="test", password="wrong")
+        self.assertFalse(user is not None and user.is_authenticated)
--- task_manager/users/tests.py
+++ task_manager/users/tests.py
@@ -1,7 +1,28 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
 
-class AppTest(TestCase):
+class UserTest(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            first_name="Ivan",
+            last_name="John",
+            username="testuser",
+            email="test@email.com",
+            password="secret",
+        )
+
     def test_availability(self):
         response = self.client.get("/users/")
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "users/user_list.html")
+
+    def test_create_user(self):
+        pass
+
+    def test_update_user(self):
+        pass
+
+    def test_delete_user(self):
+        pass
