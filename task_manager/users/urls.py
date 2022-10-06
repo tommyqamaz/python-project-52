7b140c86bba7f685728e9bd093dc5a237dceb6a8
qamaz <solgrini@yandex.ru>
2022-10-06 13:22:49 +0300

    CRUD and messages

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -1,4 +1,4 @@
-from .views import UsersView, SignUpView
+from .views import UsersView, SignUpView, DeleteUserView, UpdateUserView
 from django.urls import path
 
 
@@ -6,5 +6,7 @@ app_name = "users"
 
 urlpatterns = [
     path("", UsersView.as_view(), name="user_list"),
-    path("create", SignUpView.as_view(), name="create"),
+    path("create", SignUpView.as_view(), name="create_user"),
+    path("<int:pk>/delete", DeleteUserView.as_view(), name="delete_user"),
+    path("<int:pk>/update", UpdateUserView.as_view(), name="update_user"),
 ]
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,13 +1,20 @@
-from django.contrib.auth.models import User
 from .forms import NewUserForm
+from django.contrib.auth.models import User
 
 from django.views.generic import ListView
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic.edit import CreateView
+from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    UserPassesTestMixin,
+    AccessMixin,
+)
 
 from django.urls import reverse_lazy
-from django.contrib.messages.views import SuccessMessageMixin
-
+from django.shortcuts import redirect
 from django.contrib import messages
 from django.utils.translation import gettext_lazy as _
 
@@ -35,3 +42,44 @@ class LogoutUserView(LogoutView):
         response = super().dispatch(request, *args, **kwargs)
         messages.add_message(request, messages.INFO, _("Вы разлогинены"))
         return response
+
+
+class UpdateUserView(
+    LoginRequiredMixin, UserPassesTestMixin, SuccessMessageMixin, UpdateView
+):
+    model = User
+    success_url = reverse_lazy("users:user_list")
+    template_name = "users/update.html"
+    form_class = NewUserForm
+    success_message = _("Пользователь успешно изменён")
+    login_url = reverse_lazy("login")
+    redirect_field_name = None
+
+    def test_func(self):
+        return self.request.user.pk == self.get_object().pk
+
+    def handle_no_permission(self):
+        messages.error(
+            self.request, _("У вас нет прав для изменения другого пользователя.")
+        )
+        return redirect("users:user_list")
+
+
+class DeleteUserView(
+    LoginRequiredMixin, UserPassesTestMixin, SuccessMessageMixin, DeleteView
+):
+    model = User
+    success_url = reverse_lazy("users:user_list")
+    template_name = "users/delete.html"
+    success_message = _("Пользователь успешно удалён")
+    login_url = reverse_lazy("login")
+    redirect_field_name = None
+
+    def test_func(self):
+        return self.request.user.pk == self.get_object().pk
+
+    def handle_no_permission(self):
+        messages.error(
+            self.request, _("У вас нет прав для изменения другого пользователя.")
+        )
+        return redirect("users:user_list")
