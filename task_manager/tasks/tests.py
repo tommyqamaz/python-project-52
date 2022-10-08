ee0a34d1811ef66d3e13870b708433f1f4ca85df
qamaz <solgrini@yandex.ru>
2022-10-08 14:07:51 +0300

    listview refac

--- task_manager/tasks/tests.py
+++ task_manager/tasks/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from task_manager.users.models import MyUser
+from .models import Task
+from django.urls import reverse
 
-# Create your tests here.
+
+class TaskTest(TestCase):
+    def setUp(self):
+        self.user = MyUser.objects.create_user(
+            username="test", password="12test12", first_name="bla", last_name="blabla"
+        )
+        self.status = Task.objects.create(name="test11")
+
+    def test_status_page(self):
+        response = self.client.get(reverse("tasks:task_list"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, template_name="tasks/task_list.html")
+
+    def test_create_status(self):
+        response = self.client.post(
+            reverse("tasks:create_task"), {"name": "test_status228"}
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Task.objects.count(), 2)
+        self.assertEqual(Task.objects.last().name, "test_status228")
--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -8,16 +8,16 @@ from django.views.generic import (
 )
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Status
 from django.utils.translation import gettext_lazy as _
 
 from .forms import CreateTaskForm
+from .models import Task
 
 
 class TaskListView(LoginRequiredMixin, ListView):
-    model = Status
+    model = Task
     context_object_name = "task_list"
-    template_name = "tasks/tsks_list.html"
+    template_name = "tasks/task_list.html"
 
 
 class CreatTaskView(SuccessMessageMixin, LoginRequiredMixin, CreateView):
@@ -26,14 +26,32 @@ class CreatTaskView(SuccessMessageMixin, LoginRequiredMixin, CreateView):
     form_class = CreateTaskForm
     success_message = _("Task created successfully")
 
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.author = self.request.user
+        self.object.save()
+        return super().form_valid(form)
+
 
 class DetailTaskView(LoginRequiredMixin, DetailView):
-    pass
+    model = Task
+    template_name = "tasks/detail.html"
 
 
 class DeleteTaskView(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
-    pass
+    model = Task
+    success_url = reverse_lazy("tasks:task_list")
+    template_name = "tasks/delete.html"
+    success_message = _("Task deleted successfully")
+    login_url = reverse_lazy("login")
+    redirect_field_name = None
 
 
 class UpdateTaskView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
-    pass
+    model = Task
+    success_url = reverse_lazy("tasks:task_list")
+    template_name = "statuses/update.html"
+    form_class = CreateTaskForm
+    success_message = _("Task updated successfully")
+    login_url = reverse_lazy("tasks:task_list")
+    redirect_field_name = None
