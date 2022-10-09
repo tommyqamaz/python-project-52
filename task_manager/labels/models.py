c18a341b379063e7f733be7c22de982ac5c5fad9
qamaz <solgrini@yandex.ru>
2022-10-09 12:47:40 +0300

    tests for labels

--- task_manager/labels/models.py
+++ task_manager/labels/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class Label(models.Model):
     name = models.CharField(max_length=40)
+    created_at = models.DateTimeField(_("Creation date"), auto_now_add=True)
--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext_lazy as _
 
 class Status(models.Model):
     name = models.CharField(max_length=30, unique=True)
-    created_at = models.DateTimeField(_("Дата создания"), auto_now_add=True)
+    created_at = models.DateTimeField(_("Creation date"), auto_now_add=True)
 
     class Meta(object):
         verbose_name = "status"
